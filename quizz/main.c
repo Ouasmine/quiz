91a1c32c4b4d9a9e59fe64919bfd170a9f9f468c
Ouasmine <93976723+Ouasmine@users.noreply.github.com>
2021-11-11 11:33:14 +0100

    Add files via upload

---
+++ quizz/main.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+    {
+    printf("veuillez choisir la bonne reponse :\n");
+    int reponsejuste=0;
+    int N;
+     //int A=2,B=3 ;
+      printf("En quelle annee etait la 2ere guerre mondiale");
+      scanf("%d",&N);
+      if (N==1939){
+            reponsejuste=reponsejuste+1;
+        printf("ce nombre est juste :\n");
+      }
+
+      else
+       printf("ce nombre est faux");
+    printf("veuillez choisir la bonne reponse \n");
+    int A;
+     //int A=2,B=3 ;
+      printf("Si ahmed a 2pommes et son ami a 5 pommes combien on aurra de pommes au total ");
+      scanf("%d",&A);
+      if (A==7){
+             reponsejuste=reponsejuste+1;
+        printf("ce nombre est juste :\n");
+      }
+      else
+       printf("ce nombre est faux");
+      int G;
+      printf("En quelle annee etait la 1ere guerre mondiale");
+      scanf("%d",&G);
+      if (G==1914)  {
+          reponsejuste=reponsejuste+1;
+        printf("ce nombre est juste\n");
+      }
+
+      else
+       printf("ce nombre est faux\n");
+
+    printf("resulatat final : %d",reponsejuste);
+
+
+
+
+       return 0;
+}
